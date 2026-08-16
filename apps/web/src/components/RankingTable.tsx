f64385ac0dc82e58af8be40221cb1b0eadd0d8bf
Reddsito <ptyelnegro@gmail.com>
2026-08-16 04:25:48 -0500

    style(ranking): shorten the low elo caption to "be better"

--- apps/web/src/components/RankingTable.tsx
+++ apps/web/src/components/RankingTable.tsx
@@ -525,7 +525,7 @@ function lowEloBoundary(players: RankedPlayer[], sort: SortKey): number {
   );
 }
 
-const LOW_ELO_CAPTION = 'abandonad toda esperanza';
+const LOW_ELO_CAPTION = 'be better';
 
 /**
  * The dashes either side of the label. They fade towards the outer edge so the
